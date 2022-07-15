a3c7fad102b36fbc631bcf28148a3b1cf1c7b487
Bram Boskamp <bram@boska.mp>
2022-07-15 12:41:04 +0100

    start with netcdfreadertest

---
+++ api/src/test/java/org/fairdatapipeline/netcdf/NetcdfReaderTest.java
@@ -0,0 +1,16 @@
+package org.fairdatapipeline.netcdf;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+
+public class NetcdfReaderTest {
+
+    @Test
+    void test_read_int() {
+        String resourceName = "/netcdf/test_build_prepare_write_INT.nc";
+        String filename = getClass().getResource(resourceName).toString();
+        NetcdfReader reader = new NetcdfReader(filename);
+
+    }
+}
