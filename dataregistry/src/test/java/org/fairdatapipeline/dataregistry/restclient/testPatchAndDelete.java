ec5a1734580be00b271dd27cb9c18f9d91718696
Ryan J Field <57794045+RyanJField@users.noreply.github.com>
2023-02-22 13:49:45 +0000

    Update testPatchAndDelete.java

--- dataregistry/src/test/java/org/fairdatapipeline/dataregistry/restclient/testPatchAndDelete.java
+++ dataregistry/src/test/java/org/fairdatapipeline/dataregistry/restclient/testPatchAndDelete.java
@@ -60,6 +60,7 @@ class testPatchAndDelete {
             "outputs",
             "code_repo",
             "prov_report",
+            "ro_crate",
             "run_date",
             "updated_by",
             "uuid"));
